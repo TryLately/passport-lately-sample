8b59e20338f742fa5618a3a2bb237eec25862df5
Gregory <gregory-h@users.noreply.github.com>
2018-09-10 11:10:44 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,12 @@ module.exports = {
 	clientSecret :'',
 	callbackURL :'http://localhost:8080/auth/lately/callback',
 
-	// override passport-lately defaults to access dev.trylately.com 
+	/** uncomment to override server config  
 	serverBaseURL:'https://dev.trylately.com',
 	authorizationURL:'https://dev.trylately.com/v1/apps/oauth/authorize',
 	userProfileURL:'https://dev.trylately.com/v1/apps/user/profile',
 	tokenURL:'https://dev.trylately.com/v1/apps/oauth/token',
+	**/
 
 }; 
 
