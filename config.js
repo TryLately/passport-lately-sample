200b629b986621033536d4aebf95b7f3a7ed5292
gregory-h <greg@itactual.com>
2018-09-10 18:03:27 -0600

    bared config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
 
-	clientID :'5b96ae546d5a195ddcb09c28',
-	clientSecret :'boUAql2WUKwx8uW59DUoyHt1H2JRYThEFkj3x1dtFS',
+	clientID :'',
+	clientSecret :'',
 	callbackURL :'http://localhost:8080/auth/lately/callback',
 
 }; 
